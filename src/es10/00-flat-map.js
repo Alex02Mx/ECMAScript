e00b8e3aa309089b93811981d10d774cb5c6ec35
Alex02Mx <alexloudog34@gmail.com>
2022-12-07 05:18:52 -0600

    tercer commit

--- src/es10/00-flat-map.js
+++ src/es10/00-flat-map.js
@@ -5,6 +5,6 @@ console.log(array.flat(2));
 //----------------flatMap----------------------
 
 const array2 = [1,2,3,4,5];
-console.log(array2.flatMap(v => [v, v * 3]));
+console.log(array2.flatMap(v => [v, v * 2]));
 
 
